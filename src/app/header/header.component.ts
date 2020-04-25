9bfb5c9bba6b2d5b4c2d7bdc1fb89891b156117b
Abin Abbas <ryzm10.abin@gmail.com>
2020-04-26 04:51:06 +0530

    wave animation lifecycle hook changed

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements AfterViewInit {
 
   constructor() {}
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
     this.wave();
   }
 
