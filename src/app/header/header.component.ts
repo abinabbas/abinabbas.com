d451a951ae79f593c9fda6b3d053d4d53d9a2232
Abin Abbas <ryzm10.abin@gmail.com>
2020-04-29 22:46:46 +0530

    added touch event listner for wave animation

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,23 +1,28 @@
-import { Component, AfterViewInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent implements AfterViewInit {
+export class HeaderComponent implements OnInit, AfterViewInit {
+  private handEmoji: HTMLElement;
 
   constructor() {}
 
+  ngOnInit() {
+    this.handEmoji = document.querySelector('.wave-hand');
+  }
+
   ngAfterViewInit(): void {
     this.wave();
+    this.handEmoji.addEventListener('touchend', () => this.wave());
   }
 
-  wave(): void{
-    document.querySelector('.emoji.wave-hand').classList.add('wave');
+  wave(): void {
+    this.handEmoji.classList.add('wave');
     setTimeout(() => {
-      document.querySelector('.emoji.wave-hand').classList.remove('wave');
+      this.handEmoji.classList.remove('wave');
     }, 1000);
   }
-
 }
